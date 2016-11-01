e411c4bc124b58464ed532f95fc4402fe66514e2
Taylor Chesnutt <taylor.chesnutt@gmail.com>
2016-11-01 13:53:17 -0700

    heroku fix

--- frontend/sandcamp.jsx
+++ frontend/sandcamp.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById('root');  ReactDOM.render(<h1>suuuuh</h1>, root);
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>PLEASE GOD WURK</h1>, root);
 })
