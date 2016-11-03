3a8dae495e147a53551f84bf740cca3196bde48e
Taylor Chesnutt <taylor.chesnutt@gmail.com>
2016-11-03 09:23:29 -0700

    Removing the Minification Warning from React v15

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/sandcamp.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
