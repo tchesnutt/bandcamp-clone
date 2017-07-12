0033f9dd1ecc3069a410310825890b56cfa66e1f
Taylor Fitzgerald Chesnutt <taylor.chesnutt@gmail.com>
2017-07-12 12:35:11 -0700

    search reducer tests added

--- frontend/reducers/__tests__/search_reducer-test.js
+++ frontend/reducers/__tests__/search_reducer-test.js
@@ -54,7 +54,51 @@ describe('Reducers', () => {
         SearchReducer(oldState, action);
         expect(oldState).toEqual({ 1: 'state so old'});
       });
+    });
 
+    describe('handling the RESET_SEARCH_RESULTS action', () => {
+      let actionReceive, actionReset, payload;
+
+      beforeEach(() => {
+        payload = {
+          artists: ['artist1', 'artist2', 'artist3'],
+          albums: ['album1', 'album2', 'album3'],
+          tracks: ['track1', 'track2', 'track3'],
+          album_art: ['albumArt1', 'albumArt2', 'albumArt3']
+        }
+        actionReceive = {
+          type: 'RECIEVE_SEARCH_RESULTS',
+          query_results: payload
+        };
+        actionReset = { type: 'RESET_SEARCH_RESULTS' };
+      });
+
+      it('should clear the state', () => {
+        const blankState = SearchReducer(undefined, {});
+        const fullState = SearchReducer(blankState, actionReceive);
+        const resetState = SearchReducer(fullState, actionReset);
+        expect(resetState.result).not.toEqual(fullState.result)
+      });
+
+      it('should not modify the old state', () => {
+        const oldState = { 1: 'state so old' };
+        SearchReducer(oldState, actionReset);
+        expect(oldState).toEqual({ 1: 'state so old'});
+      });
+    });
+  });
+
+  describe('RootReducer', () => {
+    let testStore;
+
+    beforeAll(() => {
+      testStore = createStore(RootReducer);
+    });
+
+    it('includes the SearchReducer under the key `search`', () => {
+      const action = { type: 'RECIEVE_SEARCH_RESULTS' };
+      testStore.dispatch(action);
+      expect(testStore.getState().search).toEqual(SearchReducer(undefined, action));
     })
   })
 })
