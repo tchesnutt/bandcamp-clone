ecc82114c55879fce15a7623eaff112f5eada6b0
Taylor Fitzgerald Chesnutt <taylor.chesnutt@gmail.com>
2017-09-06 22:34:33 -0700

    album page media queries for small screens

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Slider from 'react-slick';
 import Album from '../album/album';
-import sizeMe from 'react-sizeme'
+import sizeMe from 'react-sizeme';
 
 const albumStyle = {
   card: {
